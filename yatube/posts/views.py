42bfbf4b49e762cb8aad7551180bd5c0afcbb909
Пименов Александр Витальевич <a.pimenov@avtodor-mc.ru>
2022-02-10 17:36:22 +0300

    profile and post_detail

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,9 @@
+from tokenize import group
 from django.core.paginator import Paginator
 
 from django.shortcuts import get_object_or_404, render
 
-from .models import Group, Post
+from .models import Group, Post, User
 
 POSTS_COUNT = 10
 
@@ -32,22 +33,23 @@ def group_posts(request, slug):
 
 
 def profile(request, username):
-    post_list = username.posts.all()
+    user = get_object_or_404(User, username=username)
+    post_list = user.posts.all()
     paginator = Paginator(post_list, POSTS_COUNT)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
+
     context = {
+        'user': user,
         'page_obj': page_obj,
     }
     return render(request, 'posts/profile.html', context)
 
 
 def post_detail(request, post_id):
-    post_list = post_id.posts.all()
-    paginator = Paginator(post_list, POSTS_COUNT)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    post = get_object_or_404(Post, post_id=post_id)
+    id = post.objects.get('pk')
     context = {
-        'page_obj': page_obj,
+        'id': id,
     }
     return render(request, 'posts/post_detail.html', context)

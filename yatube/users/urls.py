749c8fdace4331d5b8b8fd5bc63748b359753b47
Пименов Александр Витальевич <a.pimenov@avtodor-mc.ru>
2022-02-11 16:08:38 +0300

    for last exercise

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,4 +1,6 @@
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView, PasswordResetView
+from django.contrib.auth.views import PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+
 from django.urls import path
 from . import views
 
@@ -16,4 +18,28 @@ urlpatterns = [
         LoginView.as_view(template_name='users/login.html'),
         name='login'
     ),
-]
+    path(
+        'password_change/', PasswordChangeView.as_view(),
+        name='password_change'
+    ),
+    path(
+        'password_change/done/', PasswordChangeDoneView.as_view(),
+        name='password_change_done'
+    ),
+    path(
+        'password_reset/', PasswordResetView.as_view(),
+        name='password_reset_form'
+    ),
+    path(
+        'password_reset/done/', PasswordResetDoneView.as_view(),
+        name='password_reset_done'
+    ),
+    path(
+        'reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'
+    ),
+    path(
+        'reset/done/', PasswordResetCompleteView.as_view(),
+        name='password_reset_complete'
+    ),
+] 
